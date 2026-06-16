3f6d36207886fa17d8f33aacc067056a81dceb8d
basevich <basevich@proton.me>
2026-06-16 19:40:32 +0400

    add fetch option to url upload and remove fetch endpoint

--- src/index.ts
+++ src/index.ts
@@ -140,6 +140,7 @@ app.post("/url", async (c) => {
   try {
     const body = await c.req.parseBody();
     const url = body["url"];
+    const toFetch = body["fetch"];
 
     if (typeof url !== "string") {
       return c.text("missing url in request body", 400);
@@ -161,74 +162,9 @@ app.post("/url", async (c) => {
     const userhash = c.env.USERHASH;
     try {
       const urlClean = url.trim();
-      const response = await uploadUrl(urlClean, userhash);
-      const result = response.trim();
-
-      c.header("Access-Control-Allow-Origin", allowedOrigin);
-      c.header("Content-Type", "text/plain");
-
-      return c.text(result, 200);
-    } catch (err) {
-      return c.text(`catbox upload failed: ${(err as Error).message}`, 500);
-    }
-  } catch (err) {
-    return c.text(`failed to parse body: ${(err as Error).message}`, 400);
-  }
-});
-
-app.post("/fetch", async (c) => {
-  const allowedOrigin = c.env.ALLOWED_ORIGIN;
-  if (!allowedOrigin) {
-    console.error("ALLOWED_ORIGIN not set, blocking all requests");
-
-    return c.text("forbidden", 403);
-  }
-
-  const origin = c.req.header("Origin");
-  if (origin !== allowedOrigin) {
-    console.warn(`blocked request from origin: ${origin}`);
-
-    return c.text("forbidden", 403);
-  }
-
-  const contentLength = c.req.header("Content-Length");
-  if (!contentLength) {
-    return c.text("content length not specified", 400);
-  }
-
-  const maxFileSize = getMaxUploadSize(c.env);
-  const fileSize = parseInt(contentLength, 10);
-  if (fileSize > maxFileSize) {
-    return c.text("request entity too large", 413);
-  }
-
-  try {
-    const body = await c.req.parseBody();
-    const url = body["url"];
-
-    if (typeof url !== "string") {
-      return c.text("missing url in request body", 400);
-    }
-
-    if (url.length === 0) {
-      return c.text("empty url in request body", 400);
-    }
-
-    try {
-      new URL(url);
-    } catch (err) {
-      return c.text(
-        `invalid url in request body: ${(err as Error).message}`,
-        400,
-      );
-    }
-
-    const userhash = c.env.USERHASH;
-    try {
-      const urlClean = url.trim();
-      const file = await fetchFile(urlClean);
-
-      const response = await uploadBlob(file, userhash);
+      const response = toFetch
+        ? await uploadBlob(await fetchFile(urlClean), userhash)
+        : await uploadUrl(urlClean, userhash);
       const result = response.trim();
 
       c.header("Access-Control-Allow-Origin", allowedOrigin);
